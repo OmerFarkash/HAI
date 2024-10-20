6faeefde6635c84dbf84813c4a93088d1f3a2f28
Omer <omerfarkash@gmail.com>
2024-10-20 16:14:37 +0300

    attention - finish

--- Language/attention/analysis.md
+++ Language/attention/analysis.md
@@ -1,18 +1,17 @@
 # Analysis
 
-## Layer TODO, Head TODO
+## Layer 1, Head 4
 
-TODO
-
-Example Sentences:
-- TODO
-- TODO
-
-## Layer TODO, Head TODO
-
-TODO
+subject-verb relations or subject-pronoun connections
 
 Example Sentences:
-- TODO
-- TODO
+The cat slept on the sofa because it was tired.
+
+
+## Layer 3, Head 10
+
+self-preservation of tokens
+
+Example Sentences:
+The cat slept on the sofa because it was tired.
 
--- Language/attention/mask.py
+++ Language/attention/mask.py
@@ -45,8 +45,13 @@ def get_mask_token_index(mask_token_id, inputs):
     Return the index of the token with the specified `mask_token_id`, or
     `None` if not present in the `inputs`.
     """
-    # TODO: Implement this function
-    raise NotImplementedError
+    input_ids = inputs.get('input_ids').numpy().tolist()[0]
+    
+    try:
+        return input_ids.index(mask_token_id)
+    except ValueError:
+        return None
+
 
 
 
@@ -55,9 +60,9 @@ def get_color_for_attention_score(attention_score):
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
-    # TODO: Implement this function
-    raise NotImplementedError
-
+    
+    num = int(255 * attention_score)
+    return (num, num, num)
 
 
 def visualize_attentions(tokens, attentions):
@@ -70,15 +75,16 @@ def visualize_attentions(tokens, attentions):
     include both the layer number (starting count from 1) and head number
     (starting count from 1).
     """
-    # TODO: Update this function to produce diagrams for all layers and heads.
-    generate_diagram(
-        1,
-        1,
-        tokens,
-        attentions[0][0][0]
-    )
-
-
+    for layer_index in range(len(attentions)):
+        for head_index in range(len(attentions[layer_index][0])):
+            generate_diagram(
+                layer_index + 1,
+                head_index + 1,
+                tokens,
+                attentions[layer_index][0][head_index]
+            )
+            
+            
 def generate_diagram(layer_number, head_number, tokens, attention_weights):
     """
     Generate a diagram representing the self-attention scores for a single
